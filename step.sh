5bb00395d5fe8e2e41c3dd9cfae6520e37c6677c
yoko yaeg <yyaegashi@octo.com>
2015-09-08 11:58:15 +0200

    sh

---
+++ step.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "IPA path => ${BITRISE_IPA_PATH}!"
+echo "API key => ${APPALOOSA_API_KEY}!"
+echo "screen1 => ${SCREENSHOT_1}!"
+
+# return 0 as the exit code in case of success
+# return >0 if the step failed
+exit 0
