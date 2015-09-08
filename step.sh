3617b253149aef37e79f8c9fd0b14eb14222ed87
yoko yaeg <yyaegashi@octo.com>
2015-09-08 16:01:11 +0200

    sh

--- step.sh
+++ step.sh
@@ -4,6 +4,10 @@ echo "IPA path => ${BITRISE_IPA_PATH}"
 echo "API key => ${APPALOOSA_API_KEY}"
 echo "screen1 => ${SCREENSHOT_1}"
 
-# return 0 as the exit code in case of success
-# return >0 if the step failed
+export BITRISE_IPA_PATH=${BITRISE_IPA_PATH}
+export APPALOOSA_API_KEY=${APPALOOSA_API_KEY}
+export SCREENSHOT_1=${SCREENSHOT_1}
+
+env
+
 exit 0
