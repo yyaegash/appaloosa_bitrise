468af10990d241b053a87c8d4ffe4b8894e3503b
yoko yaeg <yyaegashi@octo.com>
2015-09-08 18:24:10 +0200

    sh

--- step.sh
+++ step.sh
@@ -8,10 +8,6 @@ export BITRISE_IPA_PATH=${BITRISE_IPA_PATH}
 export APPALOOSA_API_KEY=${APPALOOSA_API_KEY}
 export SCREENSHOT_1=${SCREENSHOT_1}
 
-curl -H "Content-Type: application/json" -X POST --data '{ store_id": 1, "application": { "binary_path": "http://127.0.0.1:8080/Infini.ipa", "screenshot1": ENV["SCREENSHOT_1"]}}' http://www.lvh.me:3000/api/v1/1/applications/upload?api_key=ENV['APPALOOSA_API_KEY']
-
-env 
-
-cat /etc/hosts
+curl -H "Content-Type: application/json" -X POST --data '{ store_id": 1, "appl ication": { "binary_path": "http://127.0.0.1:8080/Infini.ipa", "screenshot1": ENV["SCREENSHOT_1"]}}' http://www.lvh.me:3000/api/v1/1/applications/upload?api_key=ENV['APPALOOSA_API_KEY']
 
 exit 0
