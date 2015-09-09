b6a497454d554b1f6bdc211d6f03b3b2a957e06b
yoko yaeg <yyaegashi@octo.com>
2015-09-09 17:11:47 +0200

    modif

--- step.sh
+++ step.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "IPA path => ${BITRISE_IPA_PATH}"
+echo "IPA path => ${S3_DEPLOY_STEP_URL_IPA}"
 echo "API key => ${APPALOOSA_API_KEY}"
 # echo "screen1 => ${SCREENSHOT_1}"
 # echo "screen2 => ${SCREENSHOT_2}"
@@ -15,6 +15,7 @@ echo "store_id => ${STORE_ID}"
 
 # curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "screenshot1": "'"$SCREENSHOT_1"'","screenshot2": "'"$SCREENSHOT_2"'", "screenshot3": "'"$SCREENSHOT_3"'","screenshot4": "'"$SCREENSHOT_4"'","screenshot5": "'"$SCREENSHOT_5"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY
 
-curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY
+curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$S3_DEPLOY_STEP_URL_IPA"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY
+
 
 exit 0
