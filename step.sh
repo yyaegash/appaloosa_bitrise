d95796e4bde245a7197df920183146462d906b7e
yoko yaeg <yyaegashi@octo.com>
2015-09-09 11:21:04 +0200

    v2

--- step.sh
+++ step.sh
@@ -3,13 +3,28 @@
 echo "IPA path => ${BITRISE_IPA_PATH}"
 echo "API key => ${APPALOOSA_API_KEY}"
 echo "screen1 => ${SCREENSHOT_1}"
+echo "screen2 => ${SCREENSHOT_2}"
+echo "screen3 => ${SCREENSHOT_3}"
+echo "screen4 => ${SCREENSHOT_4}"
+echo "screen5 => ${SCREENSHOT_5}"
+echo "icon => ${ICON}"
+echo "banner => ${BANNER}"
+echo "description => ${DESCRIPTION}"
 echo "store_id => ${STORE_ID}"
 
 export BITRISE_IPA_PATH=${BITRISE_IPA_PATH}
 export APPALOOSA_API_KEY=${APPALOOSA_API_KEY}
 export SCREENSHOT_1=${SCREENSHOT_1}
+export SCREENSHOT_2=${SCREENSHOT_2}
+export SCREENSHOT_3=${SCREENSHOT_3}
+export SCREENSHOT_4=${SCREENSHOT_4}
+export SCREENSHOT_5=${SCREENSHOT_5}
+export ICON=${ICON}
+export BANNER=${BANNER}
+export DESCRIPTION=${DESCRIPTION}
 export STORE_ID=${STORE_ID}
 
-curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": ENV["BITRISE_IPA_PATH"], "screenshot1": ENV["SCREENSHOT_1"]}}' http://appaloosa-int.herokuapp.com/api/v1/ENV['STORE_ID']/applications/upload?api_key=ENV['APPALOOSA_API_KEY']
+curl -H "Content-Type: application/json" -X POST --data '{ "application": 
+{ "binary_path": "'"$BITRISE_IPA_PATH"'", "screenshot1": "'"$SCREENSHOT_1"'"}}' http://appaloosa-int.herokuapp.com/api/v1/"'"$STORE_ID"'"/applications/upload?api_key=ENV['APPALOOSA_API_KEY']
 
 exit 0
