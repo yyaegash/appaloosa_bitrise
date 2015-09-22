6004e8fe6dac0df5773536b9a7b39ae739071d99
yoko yaeg <yyaegashi@octo.com>
2015-09-22 09:57:24 +0200

    v2

--- step.sh
+++ step.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-if [ -z "${S3_DEPLOY_STEP_URL_IPA}" ] || [ -z "${APPALOOSA_API_KEY}"] || [ -z "${STORE_ID}"]; then
+echo "Variable setted"
+echo "Binary path:  ${BITRISE_IPA_PATH}"
+echo "Description:  ${DESCRIPTION}"
+echo "API Key    :  ${APPALOOSA_API_KEY}"
+
+if [ -z "${BITRISE_IPA_PATH}" ] || [ -z "${APPALOOSA_API_KEY}"] || [ -z "${STORE_ID}"]; then
 	echo "binary path, api key or store's id are missing"
 elif [ -z "${DESCRIPTION}" ]; then
-	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$S3_DEPLOY_STEP_URL_IPA"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
 else
-	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$S3_DEPLOY_STEP_URL_IPA"'", "description": "'"$DESCRIPTION"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "description": "'"$DESCRIPTION"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
 fi
 exit 0
