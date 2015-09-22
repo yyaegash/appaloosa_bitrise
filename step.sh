b2e2b4821c2424406e0c7b7e2232906320a5dfeb
yoko yaeg <yyaegashi@octo.com>
2015-09-22 11:55:11 +0200

    add group

--- step.sh
+++ step.sh
@@ -12,7 +12,9 @@ if [ -z "${BITRISE_IPA_PATH}" ] || [ -z "${APPALOOSA_API_KEY}"] || [ -z "${STORE
 	echo "binary path, api key or store's id are missing"
 elif [ -z "${DESCRIPTION}" ]; then
 	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+elif [ -z "${GROUP_IDS}" ]; then
+	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
 else
-	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "description": "'"$DESCRIPTION"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "description": "'"$DESCRIPTION"'", "group_ids": "'"$GROUP_IDS"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
 fi
 exit 0
