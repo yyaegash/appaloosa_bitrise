e341357619286ced3d538deb5c3dbd701038be8d
yoko yaeg <yyaegashi@octo.com>
2015-09-22 12:02:10 +0200

    sh

--- step.sh
+++ step.sh
@@ -3,17 +3,24 @@
 echo "Variables setted"
 echo "Binary path:  ${BITRISE_IPA_PATH}"
 echo "Description:  ${DESCRIPTION}"
+echo "GROUP_IDS  :  ${GROUP_IDS}"
 echo "API Key    :  ${APPALOOSA_API_KEY}"
-echo "API Key    :  $APPALOOSA_API_KEY"
 
 
 
 if [ -z "${BITRISE_IPA_PATH}" ] || [ -z "${APPALOOSA_API_KEY}"] || [ -z "${STORE_ID}"]; then
-	echo "binary path, api key or store's id are missing"
-elif [ -z "${DESCRIPTION}" ]; then
+	echo "Binary path, api key or store's id are missing"
+	exit -1
+
+elif [ -z "${DESCRIPTION}" ] && [ -z "${GROUP_IDS}" ] ; then
 	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+
 elif [ -z "${GROUP_IDS}" ]; then
-	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "description": "'"$DESCRIPTION"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+
+elif [ -z "${DESCRIPTION}" ] && [ -z "${GROUP_IDS}" ] ; then
+	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "group_ids": "'"$GROUP_IDS"'" }}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
+
 else
 	curl -H "Content-Type: application/json" -X POST --data '{ "application": { "binary_path": "'"$BITRISE_IPA_PATH"'", "description": "'"$DESCRIPTION"'", "group_ids": "'"$GROUP_IDS"'"}}' http://appaloosa-int.herokuapp.com/api/v1/$STORE_ID/applications/upload?api_key=$APPALOOSA_API_KEY | head
 fi
